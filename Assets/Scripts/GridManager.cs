cca18e74146e796399a810ad9ee5687ecc66404f
Yoshi-Hirai <marinos.yoshi@gmail.com>
2025-01-28 11:03:37 +0900

    背景タイルとピースオブジェクトをひとまず表示

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,27 +4,40 @@ using UnityEngine.Tilemaps;
 
 public class GridManager : MonoBehaviour
 {
-   private Tilemap m_Tilemap;
+    public int Width;
+    public int Height;
+    public Tile[] GrounPatterns;            // 背景タイルテクスチャパターン
+    public GameObject[] PiecesPatterns;      // ピースパターン 
 
-   public int Width;
-   public int Height;
-   public Tile[] GroundTiles;
+    private Tilemap m_Tilemap;
+    private Grid m_Grid;
+
+    private GameObject[,] m_Cell;      // セルデータの管理
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   void Start()
-   {
-       m_Tilemap = GetComponentInChildren<Tilemap>();
+    void Start()
+    {
+        m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_Grid = GetComponentInChildren<Grid>();
+        m_Cell = new GameObject[Width, Height];
 
-       for (int y = 0; y < Height; ++y)
-       {
-           for(int x = 0; x < Width; ++x)
-           {
-               int tileNumber = Random.Range(0, GroundTiles.Length);
-               m_Tilemap.SetTile(new Vector3Int(x, y, 0), GroundTiles[tileNumber]);
-               Debug.Log("tile: " + tileNumber);
-           }
-       }
-   }
+        for (int y = 0; y < Height; ++y)
+        {
+            for(int x = 0; x < Width; ++x)
+            {
+                //　背景タイルを生成
+                int groundIndex = Random.Range(0, GrounPatterns.Length);
+                m_Tilemap.SetTile(new Vector3Int(x, y, 0), GrounPatterns[groundIndex]);
+
+                // ピースを生成し登録
+                int pieceIndex = Random.Range(0, PiecesPatterns.Length);
+                GameObject cell = Instantiate(PiecesPatterns[pieceIndex], m_Grid.GetCellCenterWorld(new Vector3Int(x, y, 0)), Quaternion.identity);
+                m_Cell[x, y] = cell;
+
+                Debug.Log("Groudtile: " + groundIndex + " PieceTile: " + pieceIndex);
+            }
+        }
+    }
 
     // Update is called once per frame
     void Update()

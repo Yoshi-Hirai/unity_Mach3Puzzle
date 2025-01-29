d34a746db8b71c20df44050c61564fa9055a6f50
Yoshi-Hirai <marinos.yoshi@gmail.com>
2025-01-29 17:10:58 +0900

    消えて落ちるまでをいったんコミット。

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,17 +1,145 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System;
+using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour
 {
     public int Width;
     public int Height;
     public Tile[] GrounPatterns;            // 背景タイルテクスチャパターン
-    public GameObject[] PiecesPatterns;      // ピースパターン 
+    public GameObject[] PiecesPatterns;     // ピースパターン(prefab)
 
-    private Tilemap m_Tilemap;
+    private GameObject[,] m_Cell;           // セル(ピース)データの管理
+
+    private Tilemap m_Tilemap;              // 背景タイルの表示
     private Grid m_Grid;
 
-    private GameObject[,] m_Cell;      // セルデータの管理
+    // セル更新（下方向に移動して補充）
+    private void updateCell()
+    {
+        for (int x = 0; x < Width; x++)
+        {
+            // 下から上に向かって確認
+            for (int y = 0; y < Height; y++)
+            {
+                if (m_Cell[x, y] == null) // 空のスペースがある場合
+                {
+                    Debug.Log("Space: " + x + "," + y);
+                    // 現在より上部からピースを探して移動
+                    for (int aboveY = y + 1; aboveY < Height; aboveY++)
+                    {
+                        if (m_Cell[x, aboveY] != null)
+                        {
+                            Debug.Log("Bury: " + x + "," + aboveY + " => " + x + "," + y);
+                            // ピースを下に移動
+                            m_Cell[x, y] = m_Cell[x, aboveY];
+                            m_Cell[x, aboveY].transform.position = ConvertFromCellToTransform( new Vector2Int(x, y) );
+                            m_Cell[x, aboveY] = null;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // 上部から新しいピースを補充
+/*
+            for (int y = height - 1; y >= 0; y--)
+            {
+                if (grid[x, y] == null)
+                {
+                    AddPieceToGrid(x, y);
+                }
+            }
+*/
+        }
+    }
+
+    // ransform.position <=> m_cellインデックス変換
+    public Vector2Int ConvertFromTransformToCell(Vector3 transformposition)
+    {
+        return new Vector2Int( Mathf.FloorToInt(transformposition.x), Mathf.FloorToInt(transformposition.y) );
+    }
+    public Vector3 ConvertFromCellToTransform(Vector2Int cellindex)
+    {
+        return new Vector3( (float)cellindex.x + 0.5f, (float)cellindex.y + 0.5f, 0 );
+    }
+
+    // セルデータ(m_Cell)を入れ替える
+    public void SwapCellData(Vector3 pos1, Vector3 pos2)
+    {
+        // 座標をグリッドのインデックスに変換
+        Vector2Int index1 = ConvertFromTransformToCell(pos1);
+        Vector2Int index2 = ConvertFromTransformToCell(pos2);
+
+        Debug.Log("1: " + index1.x + "," + index1.y + " 2: " + index2.x + "," + index2.y);
+        // グリッドデータをスワップ
+        GameObject temp = m_Cell[index1.x, index1.y];
+        m_Cell[index1.x, index1.y] = m_Cell[index2.x, index2.y];
+        m_Cell[index2.x, index2.y] = temp;
+    }
+    
+    // 横・縦方向にピースをチェック
+    public void PieceMatchCheck()
+    {
+        List<GameObject> matchedPieces = new List<GameObject>();
+
+        // 横方向のチェック
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width - 2; x++)
+            {
+                if (m_Cell[x, y] != null &&
+                    m_Cell[x + 1, y] != null &&
+                    m_Cell[x + 2, y] != null)
+                {
+                    if (m_Cell[x, y].tag == m_Cell[x + 1, y].tag &&
+                        m_Cell[x, y].tag == m_Cell[x + 2, y].tag)
+                    {
+                        matchedPieces.Add(m_Cell[x, y]);
+                        matchedPieces.Add(m_Cell[x + 1, y]);
+                        matchedPieces.Add(m_Cell[x + 2, y]);
+                    }
+                }
+            }
+        }
+
+        // 縦方向のチェック
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height - 2; y++)
+            {
+                if (m_Cell[x, y] != null &&
+                    m_Cell[x, y + 1] != null &&
+                    m_Cell[x, y + 2] != null)
+                {
+                    if (m_Cell[x, y].tag == m_Cell[x, y + 1].tag &&
+                        m_Cell[x, y].tag == m_Cell[x, y + 2].tag)
+                    {
+                        matchedPieces.Add(m_Cell[x, y]);
+                        matchedPieces.Add(m_Cell[x, y + 1]);
+                        matchedPieces.Add(m_Cell[x, y + 2]);
+                    }
+                }
+            }
+        }
+
+        // マッチしたピースを削除
+        foreach (var piece in matchedPieces)
+        {
+            // 座標を取得
+            Vector3 pos = piece.transform.position;
+            int x = (int)Math.Floor(pos.x);
+            int y = (int)Math.Floor(pos.y);
+
+            // 先にm_CellをnullにしてからDestroy
+            m_Cell[x, y] = null;
+            Debug.Log("Delete: " + x + "," + y);
+            Destroy(piece);
+        }
+
+        updateCell();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,11 +153,11 @@ public class BoardManager : MonoBehaviour
             for(int x = 0; x < Width; ++x)
             {
                 //　背景タイルを生成
-                int groundIndex = Random.Range(0, GrounPatterns.Length);
+                int groundIndex = UnityEngine.Random.Range(0, GrounPatterns.Length);
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), GrounPatterns[groundIndex]);
 
                 // ピースを生成し登録
-                int pieceIndex = Random.Range(0, PiecesPatterns.Length);
+                int pieceIndex = UnityEngine.Random.Range(0, PiecesPatterns.Length);
                 GameObject cell = Instantiate(PiecesPatterns[pieceIndex], m_Grid.GetCellCenterWorld(new Vector3Int(x, y, 0)), Quaternion.identity);
                 m_Cell[x, y] = cell;
 
--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -1,24 +1,21 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
 public class PieceController : MonoBehaviour
 {
     private Vector2 startPos;
     private Vector2 endPos;
+
+    // マウスホバー時の明度アップ
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+
     private float swipeThreshold = 0.5f; // スワイプの判定閾値
+    private float moveSpeed = 5f; // 移動速度
 
-    void OnMouseDown()
-    {
-        startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log("MouseDown: " + startPos.x + " , " + startPos.y);
-    }
-
-    void OnMouseUp()
-    {
-        endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        DetectSwipe();
-    }
-
-    void DetectSwipe()
+    // スワイプ方向を検知する
+    private void DetectSwipe()
     {
         Vector2 swipeDelta = endPos - startPos;
 
@@ -40,6 +37,40 @@ public class PieceController : MonoBehaviour
         }
     }
 
+    // ピース移動の補間処理
+    // Vector3.Lerp を使用して位置を徐々に移動。目標位置に近づいたらループを終了し、最後に位置を固定。
+    private IEnumerator SmoothMove(Transform movingPiece, Vector3 targetPosition, float speed, System.Action onComplete)
+    {
+        while (Vector3.Distance(movingPiece.position, targetPosition) > 0.01f)
+        {
+            movingPiece.position = Vector3.Lerp(movingPiece.position, targetPosition, speed * Time.deltaTime);
+            yield return null; // 次のフレームまで待つ
+        }
+        movingPiece.position = targetPosition; // 最後に位置を固定
+
+        // コルーチン終了時にコールバックを実行
+        onComplete?.Invoke();
+    }
+
+    // ピース交換処理
+    public void SwapPosition(Transform targetPiece)
+    {
+        Vector3 originalPos = transform.position;   //  現在の位置
+        Vector3 targetPos = targetPiece.position;   //  入れ替え先の位置
+
+        // 入れ替え移動が完了したらピースマッチチェックを呼び出す
+        // [Todo] 処理負荷の懸念があるので、GameManagerなどにインスタンス化をしておく対応をする)
+        BoardManager boardManager = FindFirstObjectByType<BoardManager>();
+        if (boardManager == null) {
+            throw new Exception("BoardManager is Null.");
+        }
+
+        // 内部データ(m_cell配列)を更新
+        boardManager.SwapCellData(originalPos, targetPos);        
+        StartCoroutine(SmoothMove(transform, targetPos, moveSpeed, boardManager.PieceMatchCheck));
+        StartCoroutine(SmoothMove(targetPiece, originalPos, moveSpeed, null));
+    }
+
     void Swap(Vector2 direction)
     {
         // 入れ替え処理
@@ -58,16 +89,39 @@ public class PieceController : MonoBehaviour
         {
             // ピースを入れ替える
             Debug.Log("Swap: (" + transform.position.x + "," + transform.position.y + ") <=> (" + targetPiece.transform.position.x + "," + targetPiece.transform.position.y);
-            Vector2 originalPos = transform.position;
-            transform.position = targetPiece.transform.position;
-            targetPiece.transform.position = originalPos;
+            SwapPosition(targetPiece.transform);
         }
+    }
+
+    private void OnMouseEnter() // マウスがピースの上に乗った時
+    {
+        spriteRenderer.color = originalColor * 1.5f; // 明るくする
+    }
+
+    private void OnMouseExit() // マウスがピースから離れた時
+    {
+        spriteRenderer.color = originalColor; // 元の色に戻す
+    }
+    
+    void OnMouseDown()
+    {
+        spriteRenderer.color = originalColor * 1.25f; // 少し明るくする
+        startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log("MouseDown: " + startPos.x + " , " + startPos.y);
+    }
+
+    void OnMouseUp()
+    {
+        spriteRenderer.color = originalColor; // 元の色に戻す
+        endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        DetectSwipe();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color; // 元の色を保存
     }
 
     // Update is called once per frame
